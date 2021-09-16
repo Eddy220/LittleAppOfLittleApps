6451154fa48f0b4c24dea3aef7e63f7a031acafc
Eddy <edkim220@gmail.com>
2021-09-16 15:57:50 -0400

    littleapps carasole complete

--- index.js
+++ index.js
@@ -1,33 +1,42 @@
+// console.log('hey')
+
 let apps = [
     'app',
     'blog',
     'shop',
     'note',
-    'game'
+    'game',
+    'grave'
 ];
 
-let appName = document.getElementsById('appName')
+let appName = document.getElementById('appName')
 appName.innerText = apps[0]
 
 const toggleDark = () => {
-    document.body.style.backgroundCOlor = '#000000'
-    document.body.style.color = '#FFFFFF'
+    document.body.style.backgroundColor = 'black'
+    document.body.style.color = 'white'
+    appName.style.color = 'red'
+}
+
+const toggleLight = () => {
+    document.body.style.backgroundColor = 'white'
+    document.body.style.color = 'black'
     appName.style.color = 'red'
 }
 
 const rotateRight = () => {
     apps.push(apps.shift())
     appName.innerText = apps[0]
-    apps[0] === 'grave' ? toggleDark() : null;
+    apps[0] === 'grave' ? toggleDark() : toggleLight();
     return apps;
 }
 
 const rotateLeft = () => {
     apps.unshift(apps.pop())
     appName.innerText = apps[0]
-    apps[0] === 'grave' ? toggleDark() : null;
+    apps[0] === 'grave' ? toggleDark() : toggleLight();
     return apps;
 }
 
-document.getElementById('rightButton').onClick = rotateRight;
-document.getElementById('leftButton').onClick = rotateLeft;
+document.getElementById('rightButton').onclick = rotateRight;
+document.getElementById('leftButton').onclick = rotateLeft;
