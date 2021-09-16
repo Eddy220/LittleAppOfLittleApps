3198a37f2146cf9c1e4edd04aed1a1c8fe39e8a4
Eddy <edkim220@gmail.com>
2021-09-16 15:22:56 -0400

    carasole and darkmode implemented

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const app = [
+let apps = [
     'app',
     'blog',
     'shop',
@@ -7,3 +7,27 @@ const app = [
 ];
 
 let appName = document.getElementsById('appName')
+appName.innerText = apps[0]
+
+const toggleDark = () => {
+    document.body.style.backgroundCOlor = '#000000'
+    document.body.style.color = '#FFFFFF'
+    appName.style.color = 'red'
+}
+
+const rotateRight = () => {
+    apps.push(apps.shift())
+    appName.innerText = apps[0]
+    apps[0] === 'grave' ? toggleDark() : null;
+    return apps;
+}
+
+const rotateLeft = () => {
+    apps.unshift(apps.pop())
+    appName.innerText = apps[0]
+    apps[0] === 'grave' ? toggleDark() : null;
+    return apps;
+}
+
+document.getElementById('rightButton').onClick = rotateRight;
+document.getElementById('leftButton').onClick = rotateLeft;
